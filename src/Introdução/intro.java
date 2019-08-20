0384ed8b66c9578407ba477da51dcbf630ca35c8
ManelR10 <emanuellbarbosa@hotmail.com>
2019-08-20 08:53:06 -0300

    comentario

---
+++ src/Introdução/intro.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Introdução;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Aluno
+ */
+public class intro {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Word!");
+        int nota=5;
+        double media=7.5;
+        char conceito='A';
+        boolean aprovado=true;
+        String nome="José Alaves";
+        
+        
+        System.out.println("Aluno: "+nome+ " \nMédia:" +media );
+        
+        Scanner entrada = new Scanner(System.in);
+    }
+    
+
+
+}
